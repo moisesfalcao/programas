cac51839c54f023fd6710361346787229f047188
moises falcao <moisesfalcao@gmail.com>
2019-10-24 14:02:28 -0300

    alteração geral

--- wp-content/themes/canal/index.php
+++ wp-content/themes/canal/index.php
@@ -96,7 +96,17 @@ if ( have_posts() ) {
     //'title_li'=>'',
     //) );
 
-    
+    $terms = apply_filters( 'taxonomy-images-get-terms', '',array(
+      'parent'=>12,
+      
+    ) );
+if ( ! empty( $terms ) ) {
+    print '<ul>';
+    foreach ( (array) $terms as $term ) {
+        print '<li><a href="' . esc_url( get_term_link( $term, $term->taxonomy ) ) . '">' . wp_get_attachment_image( $term->image_id, 'detail' ) . '</a></li>';
+    }
+    print '</ul>';
+}
     ?>
 
 
